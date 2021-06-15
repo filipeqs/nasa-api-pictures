3ff0519cd5124179202169256d53d64b09eb84c7
Filipe Silva <filipeqs@outlook.com>
2021-06-15 10:28:18 -0400

    create dom elements

---
+++ script.js
@@ -0,0 +1,77 @@
+const resultsNav = document.getElementById('results-nav');
+const favoritesNav = document.getElementById('favorites-nav');
+const imagesContainer = document.querySelector('.images-container');
+const saveConfirmed = document.querySelector('.save-confirmed');
+const loader = document.querySelector('.loader');
+
+// NASA API
+const apiKey = 'DEMO_KEY';
+const count = 10;
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+const updateDOM = () => {
+    resultsArray.forEach((result) => {
+        // Card Container
+        const card = document.createElement('div');
+        card.classList.add('card');
+        // Link
+        const link = document.createElement('a');
+        link.href = result.hdurl;
+        link.title = 'View Full Image';
+        link.target = '_blank';
+        // Image
+        const image = document.createElement('img');
+        image.src = result.url;
+        image.alt = 'Nasa Picture of the Day';
+        image.loading = 'lazy';
+        image.classList.add('card-img-top');
+        // Card Body
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        // Card Title
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.textContent = result.title;
+        // Save Text
+        const saveText = document.createElement('p');
+        saveText.classList.add('clickable');
+        saveText.textContent = 'Add to Favorites';
+        // Card Text
+        const cardText = document.createElement('p');
+        cardText.textContent = result.explanation;
+        // Footer Container
+        const footer = document.createElement('small');
+        footer.classList.add('text-muted');
+        // Date
+        const date = document.createElement('strong');
+        date.textContent = result.date;
+        // Copyright
+        const copyrightResult = result.copyright || '';
+        const copyright = document.createElement('span');
+        copyright.textContent = ` ${copyrightResult}`;
+
+        // Append
+        footer.append(date, copyright);
+        cardBody.append(cardTitle, saveText, cardText, footer);
+        link.appendChild(image);
+        card.append(link, cardBody);
+        imagesContainer.appendChild(card);
+    });
+};
+
+// Get Images from NADA API
+const getNasaPictures = async () => {
+    try {
+        const response = await fetch(apiURL);
+        resultsArray = await response.json();
+
+        updateDOM();
+    } catch (error) {
+        // Catch error here
+    }
+};
+
+// On Load
+getNasaPictures();
